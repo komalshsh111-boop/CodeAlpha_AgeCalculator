cdd0154fd0a369d4c565d74e1a3e3d7d806da6a8
komalshsh111-boop <komalshsh111@gmail.com>
2026-05-11 02:15:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function age() {
+    var d1 = parseInt(document.getElementById('date').value);
+    var m1 = parseInt(document.getElementById('month').value);
+    var y1 = parseInt(document.getElementById('year').value);
+
+    var today = new Date();
+    var d2 = today.getDate();
+    var m2 = today.getMonth() + 1;
+    var y2 = today.getFullYear();
+
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (d1 > d2) {
+        d2 = d2 + month[m2 - 2];
+        m2 = m2 - 1;
+    }
+
+    if (m1 > m2) {
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+    }
+
+    var d = d2 - d1;
+    var m = m2 - m1;
+    var y = y2 - y1;
+
+    document.getElementById('age').innerHTML =
+        "Your age is " + y + " Years " + m + " Months " + d + " Days";
+}
